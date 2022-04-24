1a0f82154554b1e43fe54fbd5fb2c8d893bd19df
Devraj Sen <devrajs@valuefy.com>
2022-04-24 16:01:27 +0530

    added valid check on input

--- src/Components/GoalTracker/GoalInput/GoalForm.js
+++ src/Components/GoalTracker/GoalInput/GoalForm.js
@@ -2,6 +2,7 @@ import './GoalForm.css';
 import { useState } from 'react';
 function GoalForm(props) {
   const [enteredGoal, setEnteredGoal] = useState('');
+  const [isValid, setIsValid] = useState(true);
   function goalChangeHandler(e) {
     setEnteredGoal(e.target.value);
   }
@@ -9,19 +10,21 @@ function GoalForm(props) {
     e.preventDefault();
     let enteredValue = { id: Math.random().toString(), txt: enteredGoal };
     if (!enteredGoal.length) {
+      setIsValid(false);
       return;
     }
     props.onAddingGoal(enteredValue);
     setEnteredGoal('');
+    setIsValid(true);
   }
   return (
     <form action="" onSubmit={goalSubmitHandler}>
-      <div className="form-control">
-        <label>Create Goal</label>
-        <input type="text" value={enteredGoal} className='form-control input' onChange={goalChangeHandler} />
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
+        <label>CREATE A TO DO ACTIVITY</label>
+        <input type="text" value={enteredGoal} onChange={goalChangeHandler} />
       </div>
-      <button type="submit" className='form-control input:focus'>Add Goal</button>
-    </form>
+      <button type="submit" className='form-control input:focus'>Add Todo</button>
+    </form >
   );
 }
 export default GoalForm;
